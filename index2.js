059e176bed9ed337185c3ff103ecb9bf65fbafb5
prashant <prashant292singh@gmail.com>
2022-11-22 17:29:30 +0530

    objects

---
+++ index2.js
@@ -0,0 +1,19 @@
+let user = {
+    name: "jhon",
+    age: 19
+}
+
+let key = "name";
+// alert(user.key)//results in undefined
+// alert(user[key])//use squre bracket notaion instead
+
+let MakeUser = (name, age) => {
+    return (
+        {
+            name: name,
+            age: age
+        }
+    )
+}
+// const user1 = MakeUser("jhon",9)//making object with functions
+// console.log(user1)
