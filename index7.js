c5205aa1ee7bd2181df0ab6d53fba93a56d5cb24
prashant <prashant292singh@gmail.com>
2022-11-23 19:27:29 +0530

    promisification

--- index7.js
+++ index7.js
@@ -1,2 +1,50 @@
-// Promise API
+// Promisification
 
+// from functions that accepts callbacks to function that returns a promise
+
+function loadScript(src, callback){
+    let script = document.createElement('script')
+    script.src = src
+
+    script.onload = () => callback(null, script)
+    script.onerror = () => callback(new Error(`script load error`))
+
+    document.head.appendChild(script)
+}
+
+// let loadScriptPromise = (src) => {
+//     return new Promise((resolve, reject) => {
+//         loadScript(src, (err, script) => {
+//             if (err) reject(err)
+//             else resolve(script)
+//         })
+//     })
+// }
+
+// loadScriptPromise('./index1.js').then(res => console.log(res))
+
+
+
+//doubt in this code how does it works <ask>
+function promisify(f) {
+    return function (...args) { // return a wrapper-function (*)
+      return new Promise((resolve, reject) => {
+        function callback(err, result) { // our custom callback for f (**)
+          if (err) {
+            reject(err);
+          } else {
+            resolve(result);
+          }
+        }
+  
+        args.push(callback); // append our custom callback to the end of f arguments
+  
+        f.call(this, ...args); // call the original function
+      });
+    };
+  }
+  
+  // usage:
+  let loadScriptPromise = promisify(loadScript);
+//   loadScriptPromise(...).then(...);
+loadScriptPromise('./index1.js').then(res => console.log(res))
